90390d042118a8e18f757c39fd1069e03611fd20
Andrey <andrey9kovlev@mail.ru>
2022-03-18 12:59:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// let number = 5;
+// console.log("ef"*9);
+// const persone = `Alex`; 
+// // console.log(something);
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 23,
+//     isMarried: false
+// }
+// // console.log (obj.name)
+// console.log (obj["name"]) 
+// alert("hello")
+// const result = confirm("are you ");
+// // console.log(result);
+// const answer = +prompt("Вам есть 18?", "18")
+// console.log(answer+5)
+// const answers = [];
+// answers [0] = prompt ('как ваше имя?', '');
+// answers [1] = prompt ('как ваша фамилия?', '');
+// answers [2] = prompt ('ваше возраст?', '');
+// console.log(typeof(answers))
+// const category = 'toys';
+//  console.log(`https://someurl.com/${category}/5`)
+//  const user = 'eren';
+
+// alert (`привет,${user}`)
+console.log('arr' + "-obj")
+console.log(4 + "-obj")
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr);
+console.log(decr);
+console.log(5 % 3)
+console.log(2 * 4 === "8");
+const isChecked = true,
+    isClose = false;
+console.log(isChecked || !isClose);
+console.log(2 + 2 * 3 != 8);
